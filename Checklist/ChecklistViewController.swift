2cae83fcb96acf3246b789d6784077b3192150aa
Lea <LeaJeanFrancois@Leas-MBP.home>
2015-09-12 22:58:35 -0400

    GitHub

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChecklistViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("GitHub")
     }
 
     override func didReceiveMemoryWarning() {
